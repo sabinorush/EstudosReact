ba5902577bed6580d21b1cb5c58d52c8e9b73ca3
Gustavo Sabino <gu.sabino@hotmail.com>
2022-10-25 19:03:00 -0300

    Estrutura da aplicação - Criando componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/sabinorush.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gustavo Sabino</strong>
+                            <time title="20 de outubro às 21h21" dateTime="2022-10-22 21:21:30">Cerca 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito!! Parabénss!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
